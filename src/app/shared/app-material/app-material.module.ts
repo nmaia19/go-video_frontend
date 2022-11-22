fabecf0e3d22d46543634388dadb00b7348de9af
aline-fagundes <aline.martinsnapoli@gmail.com>
2022-11-22 16:03:01 -0300

    Criaçao parcial do header

---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
+  ]
+})
+
+export class AppMaterialModule { }
