7b5d12438363eafede259595ac24e72c1b55c1c6
Diego Oliveira Peixoto <f0fp1407@t-systems.com.br>
2022-11-25 16:04:42 -0300

    Inclusao do Angular Material e criaçao de modal de confirmaçao de exclusao

--- src/app/area-logada/admin/gerenciar-equipamentos/card-gerenciar-equipamentos/card-gerenciar-equipamentos.component.ts
+++ src/app/area-logada/admin/gerenciar-equipamentos/card-gerenciar-equipamentos/card-gerenciar-equipamentos.component.ts
@@ -1,5 +1,7 @@
 import { EquipamentoService } from './../../../../core/services/equipamento.service';
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalConfirmarExclusaoComponent } from 'src/app/area-logada/modal-confirmar-exclusao/modal-confirmar-exclusao.component';
 
 @Component({
   selector: 'app-card-gerenciar-equipamentos',
@@ -9,10 +11,22 @@ import { Component, Input } from '@angular/core';
 export class CardGerenciarEquipamentosComponent {
   @Input() equipamento: any
 
-  constructor(private service: EquipamentoService) {}
+  constructor(private service: EquipamentoService, public dialog: MatDialog) {}
 
   excluir(id: number) {
     this.service.excluir(id).subscribe()
     window.location.reload()
+  }
+
+  abrirModalExclusao(): void {
+    const dialogRef = this.dialog.open(ModalConfirmarExclusaoComponent, {
+      data: {nome: "equipamento"},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(`${result}`){
+        this.excluir(this.equipamento.id);
+      }
+    });
   }
 }
