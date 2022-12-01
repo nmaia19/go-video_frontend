13baef7c715476bb6e46ec06f33c0564325788f9
Haline Tamaoki <line.yuri3@gmail.com>
2022-12-01 15:18:40 -0300

    Adicao de cor para diferenciar equipamento indisponivel e disponivel

--- src/app/area-logada/usuario/equipamentos/card-equipamento/card-equipamento.component.ts
+++ src/app/area-logada/usuario/equipamentos/card-equipamento/card-equipamento.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-equipamento',
   templateUrl: './card-equipamento.component.html',
   styleUrls: ['./card-equipamento.component.css']
 })
-export class CardEquipamentoComponent {
+export class CardEquipamentoComponent implements OnInit{
+  indisponivelClass: string = ''
+
   @Input() equipamento: any
+
+  ngOnInit(): void {
+    if(this.equipamento.status == "INDISPONÍVEL"){
+      this.indisponivelClass = "status-equipamento-indisponivel"
+    }
+  }
 }
