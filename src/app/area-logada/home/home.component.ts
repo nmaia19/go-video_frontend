51541975b4cb23ac1c79c5868b808efd9d17289a
brunoblanquez <b.blanquez@yahoo.com.br>
2022-12-01 12:02:00 -0300

    Home dinamica para admin e colaboradores

--- src/app/area-logada/home/home.component.ts
+++ src/app/area-logada/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from 'src/app/core/services/autenticacao/token.storage.service';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  isAdmin: boolean = false
+  id: any = 0
+
+  constructor(private tokenStorage: TokenStorageService) {
+    this.isAdmin = this.tokenStorage.isAdministrador()
+    this.id = this.tokenStorage.getIdUsuario()
+  }
 
 }
