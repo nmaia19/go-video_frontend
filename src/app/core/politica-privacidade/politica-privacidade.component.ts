a1b7f2e51d84140a8fece81fb99208807acfe372
Haline Tamaoki <line.yuri3@gmail.com>
2022-12-02 08:51:29 -0300

    Correcoes no header de politicas e termos

--- src/app/core/politica-privacidade/politica-privacidade.component.ts
+++ src/app/core/politica-privacidade/politica-privacidade.component.ts
@@ -1,3 +1,4 @@
+import { TokenStorageService } from './../services/autenticacao/token.storage.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./politica-privacidade.component.css']
 })
 export class PoliticaPrivacidadeComponent {
+  estaLogado: boolean = false
 
+  constructor(private tokenStorageService: TokenStorageService){
+    if(tokenStorageService.getToken()){
+      this.estaLogado=true
+    }
+  }
 }
