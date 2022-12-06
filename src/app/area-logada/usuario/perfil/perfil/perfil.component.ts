1b6d433601f38bea4d170b7398c1dec29ef075b0
Haline Tamaoki <line.yuri3@gmail.com>
2022-12-06 15:02:15 -0300

    Refatoracao de detalhar usuario para admin conseguir vê-lo

--- src/app/area-logada/usuario/perfil/perfil/perfil.component.ts
+++ src/app/area-logada/usuario/perfil/perfil/perfil.component.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
 import { TokenStorageService } from 'src/app/core/services/autenticacao/token.storage.service';
 import { EmprestimoService } from 'src/app/core/services/emprestimo/emprestimo.service';
@@ -13,11 +13,20 @@ export class PerfilComponent {
 
   usuario: any = []
   emprestimos: any = []
+  idUsuario: any = []
+  isAdmin: boolean = false
 
-  constructor(private emprestimoService: EmprestimoService, private usuarioService: UsuarioService, private tokenService: TokenStorageService, private router: Router) {
-    const idUsuario = this.tokenService.getIdUsuario()
-    this.usuarioService.consultarPorId(idUsuario).subscribe(data => this.usuario = data)
-    this.consultarEmprestimosEncerradosPorUsuario(idUsuario)
+  constructor(private emprestimoService: EmprestimoService, private usuarioService: UsuarioService, private tokenService: TokenStorageService, private router: Router, private route: ActivatedRoute) {
+    if(!this.tokenService.isAdministrador){
+      this.idUsuario = this.tokenService.getIdUsuario()
+    }
+    else{
+      this.isAdmin = true
+      var routeParams = this.route.snapshot.paramMap
+      this.idUsuario = parseInt(routeParams.get('id') || '')
+    }
+    this.usuarioService.consultarPorId(this.idUsuario).subscribe(data => this.usuario = data)
+    this.consultarEmprestimosEncerradosPorUsuario(this.idUsuario)
   }
 
   consultarEmprestimosEncerradosPorUsuario(idUsuario: number) {
